203970bc2a59ffd2738d2dc7468a7535582387e0
Maxime <maxime.albert57@gmail.com>
2023-10-31 15:11:43 +0100

    ajout fonction interchanger pion dans armée réglage affichage score de chaque armée

--- main.py
+++ main.py
@@ -19,18 +19,17 @@ Il va y avoir 2 armées qui reprenne certaines fonctions:
 
 """
 class Armee:
+
     def __init__(self, name, pions):
+        self.LISTE_PIONS = []
         self.name = name
         self.pions = pions
-        """self.hero = hero
-        self.captain = captain
-        self.soldier = soldier"""
+        self.tt = 0
 
     def CalculerValeurTotal(self):
-        tt = 0
         for i in range(len(self.pions)):
-            tt += self.pions[i].nb * self.pions[i].value
-        return tt
+            self.tt += self.pions[i].nb * self.pions[i].value
+        return self.tt
 
     def AfficherPionsDansArmee(self):
         txt = ""
@@ -40,6 +39,20 @@ class Armee:
             texte_descrition = self.pions[i].nb * (self.pions[i].name + ":" + str(self.pions[i].value) + " ")
             txt += texte_descrition
         return txt
+
+    def CreationListePion(self):
+        if len(self.pions) == 0:
+            for i in range(0, 7):
+                self.LISTE_PIONS.append(None)
+        for pion in self.pions:
+            for i in range(pion.nb):
+                self.LISTE_PIONS.append(pion.name)
+        return self.LISTE_PIONS
+
+
+def transfert_pion_armee(lo, lj, rep_pj):
+    lo[int(rep_pj) - 1], lj[int(rep_pj) - 1] = lj[int(rep_pj) - 1], lo[int(rep_pj) - 1]
+
 
 
 # Entités des pions
@@ -51,6 +64,10 @@ soldier = Unite("Soldat", 1, 1)
 armee_ordi = Armee("Armée de l'ordi:", [hero, captain, soldier])
 armee_joueur = Armee("Armée du joueur:", [])
 
+# variables
+liste_pions_armee_ordi = armee_ordi.CreationListePion()
+liste_pions_armee_joueur = armee_joueur.CreationListePion()
+
 # lancement des méthodes
 score_tt_ordi = armee_ordi.CalculerValeurTotal()
 description_pions_ordi = armee_ordi.AfficherPionsDansArmee()
@@ -60,3 +77,16 @@ description_pions_joueur = armee_joueur.AfficherPionsDansArmee()
 # ce qui s'affichera sur le terminal:
 print(armee_ordi.name, score_tt_ordi, description_pions_ordi)
 print(armee_joueur.name, score_tt_joueurs, description_pions_joueur)
+
+# ce qui ne s'affiche pas mais que je souhaite voir pour visualiser
+print(liste_pions_armee_ordi)
+print(liste_pions_armee_joueur)
+
+# lancement boucle infini du jeu
+while True:
+    retour_joueur = input("entre un nombre du pav num: ")
+    transfert_pion_armee(liste_pions_armee_ordi, liste_pions_armee_joueur, retour_joueur)
+    print(armee_ordi.CalculerValeurTotal(), liste_pions_armee_ordi)
+    print(armee_joueur.CalculerValeurTotal(), liste_pions_armee_joueur)
+    if retour_joueur == str(0):
+        break
