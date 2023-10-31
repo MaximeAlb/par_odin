0980b1131ff617bc40cf4fbb0f61ae32ea41fe83
Maxime_MAC <maxime.albert57@gmail.com>
2023-10-31 08:50:45 +0100

    renvois du mac vers pc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # créer un repo GIT pour faire mes tests
 # réparer les bug générer en créant la class Armee
-
-"OK test push repo"
+print("c'est relou")
+"maintenant retour du mac vers pc"
 
 class Unite:
     def __init__(self, name, value, nb):
